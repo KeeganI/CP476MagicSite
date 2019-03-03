951e1270c3c9d367c5438c13f352267d6ab1cd5b
Chris Vanderhyden <31941700+ChrisVanderhyden@users.noreply.github.com>
2019-03-03 15:24:29 -0500

    js added

---
+++ Internet proj/magic.js
@@ -0,0 +1,3 @@
+function transf(x, y) { x.style = " transform: scale(1.2); transition: transform .2s; background-image: ${y};" }
+
+function revrt(x, y) { x.style = " transform: scale(1.0); transition: transform .2s;  background-image: ${y};"; }
