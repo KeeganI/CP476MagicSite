eab3e899251693cba24d032bffc76aaab4111aa9
Chris Vanderhyden <31941700+ChrisVanderhyden@users.noreply.github.com>
2019-03-03 15:59:59 -0500

    Popular cards

--- Internet proj/magic.js
+++ Internet proj/magic.js
@@ -1,3 +1,7 @@
-function transf(x, y) { x.style = " transform: scale(1.2); transition: transform .2s; background-image: ${y};" }
+function transf(x) { x.style = " transform: scale(1.2); transition: transform .2s" }
 
-function revrt(x, y) { x.style = " transform: scale(1.0); transition: transform .2s;  background-image: ${y};"; }
+function revrt(x) { x.style = " transform: scale(1.0); transition: transform .2s;" }
+
+function transf2(x) { x.style = " transform: scale(1.5); transition: transform .4s" }
+
+function revrt2(x) { x.style = " transform: scale(1.0); transition: transform .2s;" }
