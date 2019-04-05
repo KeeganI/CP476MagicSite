e0cea76a8c128c1577030502d5f367b909e88d66
William Keegan Ireland <keegan.ireland@hotmail.com>
2019-04-05 00:19:15 -0400

    Update mainpage.php
    Fixing grammer and adding a title to our video

--- MagicTheSite/mainpage.php
+++ MagicTheSite/mainpage.php
@@ -17,13 +17,15 @@
 </div>
 <br>
 <br>
+<h2> How to play! </h2>
+<br>
 <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/RZyXU1L3JXk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 <h2 style = "margin-top: 200px;">The majority of images uploaded onto this site, including images of Magic: The Gathering cards and parts thereof, are copyrighted by Wizards of the Coast.</h2>
 <br>
 <h2>Images are used to illustrate the card or part thereof in question</h2>
 <br>
 <br>
-<h2 onclick="location.href='https://company.wizards.com/';" > You can visit wizards of the coast by clicking me! </h2>
+<h2 onclick="location.href='https://company.wizards.com/';" > You can visit Wizards of the Coast by clicking me! </h2>
 
 </body>
 </html>
